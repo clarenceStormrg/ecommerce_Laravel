6f6de39da92e2a2dc0bbb96417b80f91f9242e0a
clarenceStormrg <gutierrezterronesma@gmail.com>
2026-04-08 16:04:11 -0500

    assets where added

---
+++ database/consultas.sql
@@ -0,0 +1,12 @@
+USE ecommerce;
+
+SELECT * FROM users;
+SELECT * FROM categories;
+SELECT * FROM brands;
+SELECT * FROM products;
+SELECT * FROM orders;
+SELECT * FROM order_items;
+SELECT * FROM payments;
+SELECT * FROM wishlists;
+SELECT * FROM reviews;
+SELECT * FROM shipping_addresses;
